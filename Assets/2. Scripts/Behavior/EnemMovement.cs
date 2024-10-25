7de0fb3b0e8fa0f77079352e5dd25cb1351da792
a-a <detols33@naver.com>
2024-10-26 02:03:48 +0900

    Feat. Adding MonsterSpawner

---
+++ Assets/2. Scripts/Behavior/EnemMovement.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemMovement : MonoBehaviour
+{
+    [Header("Target Object")]
+    [SerializeField] private Transform target;
+
+    [SerializeField] private float speed = 2f;
+    
+}
