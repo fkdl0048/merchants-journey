dc13fa59521aec9f4687689e85765f98c30c8e20
pakadopa <detols33@naver.com>
2024-11-07 15:26:08 +0900

    Test. Debug Code Add

---
+++ Assets/2. Scripts/Editor/GridEditor.cs
@@ -0,0 +1,16 @@
+using UnityEditor;
+
+public class GridEditor : Editor
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
