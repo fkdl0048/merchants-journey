f2045e8486b69eb232e9c9e901c07f4d8835a9d9
a-a <detols33@naver.com>
2025-01-16 16:00:24 +0900

    Feat. 아웃라인 쉐이더 추가 & 클릭 시스템 변경

--- Assets/2. Scripts/InGame/System/ClickSystem.cs
+++ Assets/2. Scripts/InGame/System/ClickSystem.cs
@@ -1,4 +1,4 @@
-using Unity.VisualScripting;
+using System.Linq;
 using UnityEngine;
 
 namespace Scripts.InGame.System
@@ -8,27 +8,12 @@ namespace Scripts.InGame.System
         public GameObject GetMouseDownGameobject(string tag)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+            RaycastHit[] hit = Physics.RaycastAll(ray);
 
-            // 디버그 레이 그리기 (씬 뷰에서만 보임)
-            Debug.DrawRay(ray.origin, ray.direction * 100f, Color.red, 1f);
-            Debug.Log($"Mouse Position: {Input.mousePosition}, Ray Origin: {ray.origin}, Ray Direction: {ray.direction}");
-
-            if (Physics.Raycast(ray, out hit))
-            {
-                Debug.Log($"Hit object: {hit.collider.gameObject.name}, Tag: {hit.collider.tag}, Position: {hit.point}");
-
-                // 유닛 선택
-                if (hit.collider.CompareTag(tag))
-                {
-                    Debug.Log("Unit selected");
-                    return hit.collider.gameObject;
-                }
-            }
-            else
-                Debug.Log("No hit detected");
-
-            return null;
+            var targetObj = hit.Where(x => x.transform.CompareTag(tag));
+            if (targetObj == null)
+                return null;
+            return targetObj.ToArray()[0].transform.gameObject;
         }
     }
 }
