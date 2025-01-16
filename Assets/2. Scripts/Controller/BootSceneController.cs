aebc40fa3467f3aee798de259d0275074b7885c6
Jeongan Lee <84510455+fkdl0048@users.noreply.github.com>
2025-01-17 04:00:58 +0900

    Feat: Title Asset Done

--- Assets/2. Scripts/Controller/BootSceneController.cs
+++ Assets/2. Scripts/Controller/BootSceneController.cs
@@ -3,17 +3,20 @@ using Scripts.Manager;
 using Scripts.Utils;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 /// <summary>
 /// boot scene에서 게임 매니저와 오디오 매니저를 초기화하고 타이틀 씬을 로드하는 클래스
 /// 이후에 여기서 게임 시작 시 나오는 로고 등을 추가
 /// </summary>
 public class BootSceneController : MonoBehaviour
-{    
+{
+    [SerializeField] private Button startButton;
+    
     void Start()
     {
         InitializeManagers();
-        //LoadTitleScene();
+        startButton?.onClick.AddListener(LoadTitleScene);
     }
     
     private void InitializeManagers()
