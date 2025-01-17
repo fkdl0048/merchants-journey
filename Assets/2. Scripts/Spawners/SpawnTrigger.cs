0e9834c9f96a3c473c63a1eac67a13bec48d7a2a
a-a <detols33@naver.com>
2025-01-18 04:19:20 +0900

    Feat. 스포너 조금 수정

--- Assets/2. Scripts/Spawners/SpawnTrigger.cs
+++ Assets/2. Scripts/Spawners/SpawnTrigger.cs
@@ -1,15 +1,27 @@
+using System.Collections;
 using UnityEngine;
 
 public class SpawnTrigger : MonoBehaviour
 {
     [Header("EnemySpawner")]
     [SerializeField] private EnemySpawnerBasedTrigger[] spawners;
+    [SerializeField] private float termSpawnTime;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Cargo") == false)
             return;
-        foreach (var spawner in spawners)
-            spawner.Spawn();
+        StartCoroutine(SpawnEnemy());
     }
+
+    private IEnumerator SpawnEnemy()
+    {
+        foreach (var spawner in spawners)
+        {
+            spawner.Spawn();
+            yield return new WaitForSeconds(termSpawnTime);
+        }
+        yield return null;
+    }
+    
 }
